ad7b2b19a2c2192c268d6a7d65a302a490c2b4b2
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2022-11-30 19:47:34 -0300

    add endpoints to retrieve, update and delete wallets

--- src/main/java/br/ufrn/imd/investbankapi/controllers/WalletController.java
+++ src/main/java/br/ufrn/imd/investbankapi/controllers/WalletController.java
@@ -1,6 +1,7 @@
 package br.ufrn.imd.investbankapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,8 +9,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +38,17 @@ public class WalletController {
         return ResponseEntity.status(HttpStatus.OK).body(walletService.findAll());
     }
 
+    @GetMapping("/{number}")
+    public ResponseEntity<Object> getOneWallet(@PathVariable(value = "number") int number) {
+        Optional<Wallet> walletOptional = walletService.findByNumber(number);
+
+        if (!walletOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Wallet with number %s not found.", number));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(walletOptional.get());
+    }
+
     @PostMapping
     public ResponseEntity<Object> createWallet(@RequestBody @Valid WalletDto walletDto) {
         if (walletService.existsByNumber(walletDto.getNumber())) {
@@ -45,6 +60,35 @@ public class WalletController {
         BeanUtils.copyProperties(walletDto, wallet);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(walletService.save(wallet));
+    }
+
+    @PutMapping("/{number}")
+    public ResponseEntity<Object> updateWallet(@PathVariable(value = "number") int number, @RequestBody @Valid WalletDto walletDto) {
+        Optional<Wallet> walletOptional = walletService.findByNumber(number);
+
+        if (!walletOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Wallet with number %s not found.", number));
+        }
+
+        var wallet = walletOptional.get();
+
+        wallet.setNumber(walletDto.getNumber());
+        wallet.setOwner(walletDto.getOwner());
+
+        return ResponseEntity.status(HttpStatus.OK).body(walletService.save(wallet));
+    }
+
+    @DeleteMapping("/{number}")
+    public ResponseEntity<Object> deleteWallet(@PathVariable(value = "number") int number) {
+        Optional<Wallet> walletOptional = walletService.findByNumber(number);
+
+        if (!walletOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Wallet with number %s not found.", number));
+        }
+
+        walletService.delete(walletOptional.get());
+        
+        return ResponseEntity.status(HttpStatus.OK).body(String.format("Wallet with number %s successfully deleted!", number));
     }
 
 }
