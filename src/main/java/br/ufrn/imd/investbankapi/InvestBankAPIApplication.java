e222e3a0782f2d74c631c0e6bdead462bf3cac3d
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2022-12-06 12:09:58 -0300

    add index content

--- src/main/java/br/ufrn/imd/investbankapi/InvestBankAPIApplication.java
+++ src/main/java/br/ufrn/imd/investbankapi/InvestBankAPIApplication.java
@@ -1,7 +1,12 @@
 package br.ufrn.imd.investbankapi;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +19,42 @@ public class InvestBankAPIApplication {
 	}
 
 	@GetMapping("/")
-	public String index(){
-		
-		return "Hello World!";
+	public ResponseEntity<LinkedHashMap<String, Object>> index(){
+		LinkedHashMap<String, Object> content = new LinkedHashMap<String, Object>();
+
+		content.put("project", "Invest Bank API");
+    	content.put("version", "v1");
+
+		ArrayList<String> technologies = new ArrayList<String>();
+		technologies.add("Java");
+		technologies.add("Spring Boot");
+		technologies.add("Postgres");
+		technologies.add("Docker");
+
+		content.put("technologies", technologies);
+
+		LinkedHashMap<String, String> student = new LinkedHashMap<String, String>();
+		student.put("name", "Rary Coringa");
+		student.put("email", "rary.goncalves.123@ufrn.edu.br");
+		student.put("id", "20210081823");
+
+		content.put("student", student);
+
+		LinkedHashMap<String, String> university_class = new LinkedHashMap<String, String>();
+		university_class.put("name", "Programming Language II - IMD0040");
+		university_class.put("course", "Information Technology");
+		university_class.put("degree", "Bachelor");
+		university_class.put("department", "Metrópole Digital Institute");
+		university_class.put("university", "Federal University of the Rio Grande do Norte");
+
+		content.put("class", university_class);
+
+		LinkedHashMap<String, String> professor = new LinkedHashMap<String, String>();
+		professor.put("name", "Itamir Filho");
+		professor.put("email", "itamir.filho@imd.ufrn.br");
+
+		content.put("professor", professor);
+
+		return ResponseEntity.status(HttpStatus.OK).body(content);
 	}
 }
